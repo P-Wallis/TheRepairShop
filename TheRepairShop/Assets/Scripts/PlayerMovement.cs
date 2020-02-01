718562037d6809f25e121d1481583e9091779199
Peter Wallis <p.a.e.wallis@gmail.com>
2020-02-01 11:09:29 -0600

    Basic Player Movement (on the the xz-plane)

--- TheRepairShop/Assets/Scripts/PlayerMovement.cs
+++ TheRepairShop/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(0, 50)] public float m_speed = 20;
+    const string c_horizontalAxis = "Horizontal";
+    const string c_verticalAxis = "Vertical";
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(Input.GetAxis(c_horizontalAxis), 0, Input.GetAxis(c_verticalAxis));
+        bool isMoving = false;
+
+        if (movement.sqrMagnitude > 0.0001f)
+        {
+            isMoving = true;
+            movement = movement.normalized;
+        }
+
+        if (isMoving)
+        {
+            transform.Translate(movement * (m_speed * Time.deltaTime));
+        }
     }
 }
