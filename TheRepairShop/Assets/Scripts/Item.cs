e1dfb40c9c67df2991c9397a1251161040b402cd
Peter Wallis <p.a.e.wallis@gmail.com>
2020-02-01 13:37:02 -0600

    Created Carpenter Region
    * Created worker region
    * Added work types
    * Done region now requires "NONE" work type

--- TheRepairShop/Assets/Scripts/Item.cs
+++ TheRepairShop/Assets/Scripts/Item.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public string m_name;
+    [SerializeField] private WorkType m_workRequired;
+
+    public WorkType GetRequiredWork()
+    {
+        return m_workRequired;
+    }
+
+    public void WorkDone()
+    {
+        m_workRequired = WorkType.NONE;
+    }
 }
--- TheRepairShop/Assets/Scripts/Regions/OutRegion.cs
+++ TheRepairShop/Assets/Scripts/Regions/OutRegion.cs
@@ -8,13 +8,14 @@ public class OutRegion : RegionBase
 
     public override Item ItemInteraction(Item playerItem)
     {
-        if (playerItem != null)
+        if (playerItem != null && playerItem.GetRequiredWork() == WorkType.NONE)
         {
             m_items.Add(playerItem);
             playerItem.transform.parent = transform;
             playerItem.transform.localPosition = Vector3.up * (m_items.Count);
+            return null;
         }
 
-        return null;
+        return playerItem;
     }
 }
