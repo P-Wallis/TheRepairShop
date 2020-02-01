ba138ffef2676a792cbbcd4cf607b192552b5964
Peter Wallis <p.a.e.wallis@gmail.com>
2020-02-01 12:40:26 -0600

    Region Setup

---
+++ TheRepairShop/Assets/Scripts/PlayerItemInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemInteraction : MonoBehaviour
+{
+    RegionBase m_currentRegion = null;
+    Item m_heldItem = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == RegionBase.RegionTag)
+        {
+            m_currentRegion = other.GetComponent<RegionBase>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == RegionBase.RegionTag)
+        {
+            RegionBase otherRegion = other.GetComponent<RegionBase>();
+            if (otherRegion == m_currentRegion)
+                m_currentRegion = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && m_currentRegion != null)
+        {
+            m_heldItem = m_currentRegion.ItemInteraction(m_heldItem);
+            Debug.Log("The player is now holding '" + m_heldItem.m_name+"'");
+        }
+    }
+}
