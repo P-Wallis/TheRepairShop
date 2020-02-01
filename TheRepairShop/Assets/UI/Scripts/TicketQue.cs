e3ff5e4d16778f2118e4559dde67a9eb1bfa608c
Jason <jason.r.bennett82@gmail.com>
2020-02-01 13:17:28 -0600

    Three Birdhouses
    Added a Green and Yellow Birdhouse for testing.
    
    Also started TicketQue script

---
+++ TheRepairShop/Assets/UI/Scripts/TicketQue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketQue : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
