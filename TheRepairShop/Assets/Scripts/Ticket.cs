06f21b938cc280db4ef989cc3743f9fa89df316c
Jason <jason.r.bennett82@gmail.com>
2020-02-01 19:20:36 -0600

    Ticket sound added

--- TheRepairShop/Assets/Scripts/Ticket.cs
+++ TheRepairShop/Assets/Scripts/Ticket.cs
@@ -17,6 +17,8 @@ public class Ticket : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        AudioPlayer.Instance.PlayAudioOnce("Bell1");
+
         InitializeVars();
 
         AssignRandomImage();
