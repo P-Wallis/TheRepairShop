01c0c1b5b28c55d8a824f7df04eec92e5b14da86
Peter Wallis <p.a.e.wallis@gmail.com>
2020-02-02 10:14:41 -0600

    Reputation Meter Can Now Display Rep in Stars.

--- TheRepairShop/Assets/Scripts/ReputationMeter.cs
+++ TheRepairShop/Assets/Scripts/ReputationMeter.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReputationMeter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image[] m_stars;
+    public Sprite m_starFull, m_starHalf, m_starEmpty;
+
+    float m_currentRep;
+    void SetMeter(float value)
     {
-        
+        for (int i = 0; i < m_stars.Length; i++)
+        {
+            if (value > (i + .75f))
+                m_stars[i].sprite = m_starFull;
+            else if (value < (i + .25f))
+                m_stars[i].sprite = m_starEmpty;
+            else
+                m_stars[i].sprite = m_starHalf;
+        }
+        m_currentRep = value;
+        Debug.Log("Rep: " + m_currentRep);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SetMeter(Random.Range(0f, 5f));
     }
 }
